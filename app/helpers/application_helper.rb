89624354aa9cabb845569776bba2e682521e0718
Rupesh M <rupeshcool208@gmail.com>
2024-02-06 11:00:10 +0530

    helper method in application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id] # if user id exists
-  end
+  
 
-  def logged_in?
-    !!current_user # boolean
-  end
+
 end
